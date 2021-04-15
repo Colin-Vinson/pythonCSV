811fc5495be62d41489b41359ae66ade06aea598
Colin Vinson <colin.vinson@osloskolen.no>
2021-04-15 13:31:33 +0200

    added CSVWrite.py and newCSV.csv

--- CSVwrite.py
+++ CSVwrite.py
@@ -1,8 +1,9 @@
 import csv
 
-with open('employee_file.csv', mode='w', newline='') as employee_file:
-    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+with open('newCSV.csv', mode='w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    employee_writer.writerow(['Name', 'Dept', 'Birth_month'])
-    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
-    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+    csv_writer.writerow(['Name', 'Dept', 'Birth_month'])
+    csv_writer.writerow(['John Smith', 'Accounting', 'November'])
+    csv_writer.writerow(['Erica Meyers', 'IT', 'March'])
+    csv_writer.writerow(['Eric Edward', 'IT', 'April']
