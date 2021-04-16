8c6ed5e464e97eafb6898a6e40fdedb4f3003bb4
Colin Vinson <colin.vinson@osloskolen.no>
2021-04-16 12:26:38 +0200

    added employee id#

--- csvFind.py
+++ csvFind.py
@@ -4,14 +4,14 @@ with open('hrdata_modified.csv') as f:
     reader = csv.reader(f)
     
     found = 0
-    success = 0
+    #success = 0
 
     employee = input('Enter employee name: ')
     
     for row in reader:
         if employee in row[0]:
             found = 1
-            # if row[3] == '15':
+            # if row[4] == '99-999-9999':
             #     success = 1
     if found == 1:
         print('Employee found')
