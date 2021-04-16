6d8f516de987c400a808e2a46f3d2a5abb60e967
Colin Vinson <colin.vinson@osloskolen.no>
2021-04-16 11:36:57 +0200

    new variable name

--- CSVpandasAddRow.py
+++ CSVpandasAddRow.py
@@ -2,7 +2,8 @@ import pandas
 
 df = pandas.read_csv('hrdata_modified.csv')
 
-newEntry = ['Eric Edward','2014-06-23',71000.00,5]
-df.loc[len(df.index)+1] = newEntry
+newEmployee = ['Eric Edward','2014-06-23',71000.00,5]
+
+df.loc[len(df.index)+1] = newEmployee
 
 df.to_csv('hrdata_modified.csv', index=False)
