0f9b8e441bf59c24ed8c0d27d68878461219959c
Colin Vinson <colin.vinson@osloskolen.no>
2021-04-16 13:51:59 +0200

    created python file

---
+++ CSVchangeValue.py
@@ -0,0 +1,24 @@
+import csv
+
+user_input=input('Enter employee name: ')
+
+def find_index(input): 
+    o = open('hrdata_modified.csv', 'r') 
+    myData = csv.reader(o) 
+    index = 0 
+    for row in myData:
+      #print (row)
+      if row[0] == input: 
+        return index 
+      else : index+=1
+    o.close()  
+val = find_index(user_input)
+print (val)
+
+r = csv.reader(open('hrdata_modified.csv')) 
+lines = list(r)
+
+lines[val][4] = '88-888-8888'
+
+writer = csv.writer(open('output.csv', 'w'))
+writer.writerows(lines)
